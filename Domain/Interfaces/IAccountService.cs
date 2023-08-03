24c9ebda5f5e1e4313fdc67cdcbc7f5c6cec0af2
J-S-James <jordanjames_tmo@hotmail.com>
2023-08-03 22:31:05 +0100

    IAccountService interface class

---
+++ Domain/Interfaces/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces;
+public interface IAccountService
+{
+    Task<Account> CreateAccountAsync(Account account);
+    Task<Account> GetAccountAsync(Account account);
+    Task<Account> UpdateAccountAsync(Account account);
+    Task<Account> DeleteAccountAsync(Account account);
+}
