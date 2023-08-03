b444b66868d0192351802fb4da619e491dc97267
J-S-James <jordanjames_tmo@hotmail.com>
2023-08-03 22:30:20 +0100

    Update IAccountRepo

--- Domain/Interfaces/IAccountRepo.cs
+++ Domain/Interfaces/IAccountRepo.cs
@@ -3,5 +3,8 @@
 namespace Domain.Interfaces;
 public interface IAccountRepo
 {
+    Task<int> CreateAsync(Account account);
     Task<Account?> GetAsync(Account account);
+    Task<int> UpdateAsync(Account account);
+    Task<int> DeleteAsync(Account account);
 }
